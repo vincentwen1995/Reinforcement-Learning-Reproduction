bad4b7ae90c6012af1285a4d36bc034c105e1322
philip <philip.yijie.zhang@gmail.com>
2019-10-08 14:47:46 +0200

    create A2C

--- GAE/__main__.py
+++ GAE/__main__.py
@@ -9,7 +9,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from torch.distributions import Categorical
-from GAE.model import MC
+from GAE.model import MC, A2C
 
 
 def main():
@@ -17,9 +17,9 @@ def main():
     parser.add_argument('--env', type=str, default='CartPole-v0', help='environment to experiment (default=CartPole-v0)')
     parser.add_argument('--same_network', type=bool, default=True, help='flag for using the same NN for both actor and critic')
     parser.add_argument('--num_hidden', type=int, default=128, help='number of hidden units in the linear layers')
-    parser.add_argument('--episodes', type=int, default=500, help='number of episodes (default=50)')
-    parser.add_argument('--max_steps', type=int, default=10000, help='maximum number of steps allowed for each episode')
-    parser.add_argument('--lr', type=float, default=1e-2, help='learning rate (default: 1e-2)')
+    parser.add_argument('--episodes', type=int, default=1000, help='number of episodes (default=50)')
+    parser.add_argument('--step_rollouts', type=int, default=10, help='step size for rollouts, n')
+    parser.add_argument('--lr', type=float, default=1e-3, help='learning rate (default: 1e-2)')
     parser.add_argument('--gamma', type=float, default=0.99,
                         help='discount factor (default: 0.99)')
     parser.add_argument('--seed', type=int, default=42,
@@ -32,8 +32,8 @@ def main():
 
     args = parser.parse_args()
 
-    monte_carlo = MC(args)
-    monte_carlo.train()
+    a2c = A2C(args)
+    a2c.train()
 
 
 if __name__ == '__main__':
--- GAE/model.py
+++ GAE/model.py
@@ -4,6 +4,7 @@ from gym import spaces
 import numpy as np
 from itertools import count
 from collections import namedtuple
+import copy
 
 import torch
 import torch.nn as nn
@@ -47,8 +48,12 @@ class MC(BaseMethod):
         action_prob, state_value = self.model(state)
 
         action_dist = Categorical(action_prob)
+        #print(action_prob)
         action = action_dist.sample()
 
+        #print(action.item())
+        #print(action_dist.log_prob(action))
+        #print(state_value)
         return action.item(), action_dist.log_prob(action), state_value
 
     def update(self):
@@ -63,11 +68,17 @@ class MC(BaseMethod):
             returns.insert(0, ret)
 
         returns = torch.tensor(returns)
-        returns = (returns - returns.mean()) / (returns.std() + EPSILON)
-
-        for step, ret in zip(self.trajectory, returns):
+        #print("returns: ", returns)
+        #returns = (returns - returns.mean()) / (returns.std() + EPSILON)
+        
+        final_v = self.trajectory[-1][1][-1]
+        #print(final_v)
+        for step, ret, i in zip(self.trajectory, returns, range(len(returns))):
             _, log_prob, value = step[1]
-            advantage = ret - value.item()
+            advantage = ret + self.args.gamma**(self.args.step_rollouts-i) * final_v.item() - value.item()
+            #print("ret: ", ret)
+            #advantage = ret - value.item()
+
 
             # calculate actor (policy) loss
             actor_losses.append(-log_prob * advantage)
@@ -80,6 +91,7 @@ class MC(BaseMethod):
 
         # perform backprop
         loss.backward()
+        #torch.nn.utils.clip_grad_norm(self.model.parameters(),0.5)
 
     def train(self):
 
@@ -103,42 +115,195 @@ class MC(BaseMethod):
             # reset environment and episode reward
             state = env.reset()
             ep_reward = 0
+            done = False
 
             # for each episode, only run 9999 steps so that we don't
             # infinite loop while learning
-            self.trajectory = []
-            for t in np.arange(1, self.args.max_steps):
+            
+            while not done:
+                
+                self.trajectory = []
+                for t in np.arange(self.args.step_rollouts):
 
-                # select action from policy
-                action, log_prob, state_value = self.select_action(state)
+                    # select action from policy
+                    action, log_prob, state_value = self.select_action(state)
 
-                # take the action
-                next_state, reward, done, _ = env.step(action)
-                self.trajectory.append((state, (action, log_prob, state_value), reward))
-                state = next_state
+                    # take the action
+                    next_state, reward, done, _ = env.step(action)
+                    self.trajectory.append((state, (action, log_prob, state_value), reward))
 
-                if self.args.render:
-                    env.render()
+                    ep_reward += reward
 
-                ep_reward += reward
-                if done:
-                    break
+                    if done:
+                        break
 
-            # update cumulative reward
-            running_reward = 0.05 * ep_reward + (1 - 0.05) * running_reward
+                    state = next_state
 
-            # perform backprop
-            optimizer.zero_grad()
-            self.update()
-            optimizer.step()
+                    if self.args.render:
+                        env.render()
 
+                    
+
+
+                # update cumulative reward
+                running_reward = 0.05 * ep_reward + (1 - 0.05) * running_reward
+
+                # perform backprop
+                optimizer.zero_grad()
+                self.update()
+                optimizer.step()
+            
             # log results
             if i_episode % self.args.log_interval == 0:
+
+                test_env = copy.copy(env)
+                test_rewards = []
+
+                for _ in range(10):
+                    test_state = env.reset()
+                    test_ep_reward = 0
+                    test_done = False
+
+                    while not test_done:
+                        test_action, _, _ = self.select_action(test_state)
+                        test_state, test_reward, test_done, _ = env.step(test_action)
+                        test_ep_reward += test_reward
+                        if test_done:
+                            break
+                    test_rewards.append(test_ep_reward)
+                test_avg_reward = np.mean(test_rewards)
                 print('Episode {}\tLast reward: {:.3f}\tAverage reward: {:.3f}'.format(
-                    i_episode, ep_reward, running_reward))
+                    i_episode, ep_reward, test_avg_reward))
 
             # check if we have "solved" the cart pole problem
-            if running_reward > env.spec.reward_threshold:
+            if test_avg_reward > env.spec.reward_threshold:
                 print("Solved! Running reward is now {} and "
-                      "the last episode runs to {} time steps!".format(running_reward, t))
+                      "the last episode runs to {} time steps!".format(test_avg_reward, t))
                 break
+
+
+class A2C(BaseMethod):
+
+    def select_action(self, state):
+        state = torch.tensor(state, dtype=torch.float, device=self.args.device)
+        action_prob, state_value = self.model(state)
+
+        action_dist = Categorical(action_prob)
+        action = action_dist.sample()
+
+        return action.item(), action_dist.log_prob(action), state_value
+
+    def update(self):
+        actor_losses = []
+        critic_losses = []
+        returns = []
+
+        # calculate cumulated discounted rewards
+        ret = 0
+        for step in self.trajectory[::-1]:
+            reward = step[2]
+            ret = reward + self.args.gamma * ret
+            returns.insert(0, ret)
+
+        returns = torch.tensor(returns)
+        
+
+        final_v = self.trajectory[-1][1][-1]
+        for step, ret, i in zip(self.trajectory, returns, range(len(returns))):
+            _, log_prob, value = step[1]
+            q = ret + self.args.gamma**(self.args.step_rollouts-i) * final_v.item()
+            v = value.item()
+            advantage = q - v 
+            actor_losses.append(-log_prob * advantage)
+            # calculate critic (value) loss using L1 smooth loss
+            critic_losses.append(F.smooth_l1_loss(value, torch.tensor([ret], device=self.args.device)))
+
+        # sum up all the values of actor_losses and critic_losses
+        loss = torch.stack(actor_losses).sum() + torch.stack(critic_losses).sum()
+
+        # perform backprop
+        loss.backward()
+        #torch.nn.utils.clip_grad_norm(self.model.parameters(),0.5)
+
+    def train(self):
+
+        env = gym.make(self.args.env)
+        env.seed(self.args.seed)
+        torch.manual_seed(self.args.seed)
+
+        self.model = Policy(
+            num_obs=get_space_shape(env.observation_space),
+            num_actions=get_space_shape(env.action_space),
+            same_network=self.args.same_network,
+            num_hidden=self.args.num_hidden
+        )
+        self.model.to(self.args.device)
+        optimizer = optim.Adam(self.model.parameters(), lr=self.args.lr)
+
+        for i_episode in np.arange(self.args.episodes):
+
+            # reset environment and episode reward
+            state = env.reset()
+            ep_reward = 0
+            done = False
+
+            # for each episode, only run 9999 steps so that we don't
+            # infinite loop while learning
+            
+            while not done:
+                
+                self.trajectory = []
+                # start n-step rollouts 
+                for _ in np.arange(self.args.step_rollouts):
+
+                    # select action from policy
+                    action, log_prob, state_value = self.select_action(state)
+
+                    # take the action
+                    next_state, reward, done, _ = env.step(action)
+                    self.trajectory.append((state, (action, log_prob, state_value), reward))
+
+                    ep_reward += reward
+
+                    if done:
+                        break
+
+                    state = next_state
+
+                # perform backprop
+                optimizer.zero_grad()
+                self.update()
+                optimizer.step()
+            
+            # log results
+            if i_episode % self.args.log_interval == 0:
+                
+                # tests
+                test_env = copy.copy(env)
+                test_rewards = []
+
+                for _ in range(10):
+                    test_state = env.reset()
+                    test_ep_reward = 0
+                    test_done = False
+
+                    while not test_done:
+                        test_action, _, _ = self.select_action(test_state)
+                        test_state, test_reward, test_done, _ = env.step(test_action)
+                        test_ep_reward += test_reward
+                        
+                        if test_done:
+                            break
+
+                    test_rewards.append(test_ep_reward)
+                
+                test_avg_reward = np.mean(test_rewards)
+                
+                print('Episode {}\tLast training reward: {:.3f}\tAverage test reward: {:.3f}'.format(
+                    i_episode, ep_reward, test_avg_reward))
+
+            # check if we have "solved" the cart pole problem
+            if test_avg_reward > env.spec.reward_threshold:
+                print("Solved! Average test reward is now {}!".format(test_avg_reward))
+                break
+            
