de80a12faf1aed93f29ab51d241f8b6ddf12acb9
Vincent Wen <x.wen@student.tue.nl>
2019-10-13 21:37:23 +0200

    Update the test.py.

--- GAE/test.py
+++ GAE/test.py
@@ -9,17 +9,18 @@ import random
 import os
 import gym
 import copy
+from torch.distributions import Categorical
 
 # experiment parameters
-STEP = 100000  # total training steps
+STEP = 10000  # total training steps
 TEST_INTERVAL = 100
 N_TEST_EPI = 50
-
-#ENV = "CartPole-v0" # envs: 
-#SAMPLE_NUMS = 1  # n-step boostrapping: 1, 10, 40, 80, 120, mc 
-#SEED = 42  # random seed: [42, 36, 15]
+DEVICE = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+HIDDEN_SIZE = 64
+# ENV = "CartPole-v0" # envs:
+# STEP_ROLLOUT = 1  # n-step boostrapping: 1, 10, 40, 80, 120, mc
+# SEED = 42  # random seed: [42, 36, 15]
 # Hyper Parameters
-
 
 
 class ActorNetwork(nn.Module):
@@ -33,7 +34,7 @@ class ActorNetwork(nn.Module):
     def forward(self, x):
         out = F.relu(self.fc1(x))
         out = F.relu(self.fc2(out))
-        out = F.log_softmax(self.fc3(out))
+        out = F.log_softmax(self.fc3(out), dim=-1)
         return out
 
 
@@ -54,12 +55,13 @@ class ValueNetwork(nn.Module):
 
 def roll_out(actor_network,
              value_network,
-             task,
-             sample_nums,
-             init_state):
+             env,
+             step_rollout,
+             init_state,
+             action_dim):
     ''' Collecting data for n-step rollouts
     '''
-    #task.reset()
+    # env.reset()
     states = []
     actions = []
     rewards = []
@@ -67,17 +69,21 @@ def roll_out(actor_network,
     v_final_state = 0  # value for final state
     state = init_state
 
-    for _ in range(sample_nums):
+    for _ in range(step_rollout):
 
         # action
-        log_softmax_action = actor_network(Variable(torch.Tensor([state])))
+        log_softmax_action = actor_network(torch.tensor([state], dtype=torch.float, device=DEVICE))
         softmax_action = torch.exp(log_softmax_action)
-        action = np.random.choice(
-            ACTION_DIM, p=softmax_action.cpu().data.numpy()[0])
-        one_hot_action = [int(k == action) for k in range(ACTION_DIM)]
+
+        action_dist = Categorical(softmax_action)
+        action = action_dist.sample()
+        # action = np.random.choice(
+        # action_dim, p=softmax_action.cpu().data.numpy()[0])
+        # one_hot_action = [int(k == action) for k in range(action_dim)]
+        one_hot_action = [int(k == action.item()) for k in range(action_dim)]
 
         # take action
-        next_state, reward, done, _ = task.step(action)
+        next_state, reward, done, _ = env.step(action.item())
 
         # collect rollouts data
         states.append(state)
@@ -89,12 +95,12 @@ def roll_out(actor_network,
 
         if done:
             is_done = True
-            state = task.reset()
+            state = env.reset()
             break
 
     if not is_done:
         v_final_state = value_network(
-            Variable(torch.Tensor([final_state]))).cpu().data.numpy()
+            torch.tensor([final_state], dtype=torch.float, device=DEVICE)).cpu().data.numpy()
 
     return states, actions, rewards, v_final_state, state
 
@@ -109,35 +115,42 @@ def discount_reward(r, gamma, v_final_state):
 
 
 def main():
-    
-    for ENV in ["CartPole-v0"]:
-        STATE_DIM = 4
-        ACTION_DIM = 2
-        for SEED in [42, 36, 15]:
-            for SAMPLE_NUMS in [1, 10, 40, 80, 150, 200]:
 
-                result_name = str(SEED) + "_" + str(SAMPLE_NUMS) + ".npy"
+    # for ENV, STATE_DIM, ACTION_DIM, STEP_ROLLOUTS in zip(["CartPole-v0", "MountainCar-v0", 'Acrobot-v1'], [4, 2, 6], [2, 3, 3], [[1, 10, 40, 80, 150, 200], [1, 10, 40, 80, 150, 200], [1, 30, 60, 100, 300, 500]]):
+    # for ENV, STATE_DIM, ACTION_DIM, STEP_ROLLOUTS in zip(["MountainCar-v0"], [2], [3], [[1, 10, 40, 80, 150, 200]]):
+    for ENV, STATE_DIM, ACTION_DIM, STEP_ROLLOUTS in zip(['Acrobot-v1'], [6], [3], [[1, 30, 60, 100, 300, 500]]):
+        # STATE_DIM = 4
+        # ACTION_DIM = 2
+        print('Env: {}'.format(ENV))
+        for SEED in [42, 36, 15]:
+            print('Seed: {}'.format(SEED))
+            for STEP_ROLLOUT in STEP_ROLLOUTS:
+                print('Step Rollout: {}'.format(STEP_ROLLOUT))
+
+                result_file_name = 'results/' + ENV + '_' + str(SEED) + "_" + str(STEP_ROLLOUT) + ".npy"
 
                 # training env
-                task = gym.make(ENV)
-                task.seed(SEED)
-                init_state = task.reset()
+                env = gym.make(ENV)
+                env.seed(SEED)
+                init_state = env.reset()
 
                 # test env
-                test_task = copy.deepcopy(task)
-                test_task.seed(SEED)
+                test_env = copy.deepcopy(env)
+                test_env.seed(SEED)
 
                 torch.manual_seed(SEED)
                 np.random.seed(SEED)
 
                 # init value network
                 value_network = ValueNetwork(
-                    input_size=STATE_DIM, hidden_size=40, output_size=1)
+                    input_size=STATE_DIM, hidden_size=HIDDEN_SIZE, output_size=1)
+                value_network.to(DEVICE)
                 value_network_optim = torch.optim.Adam(
                     value_network.parameters(), lr=0.001)
 
                 # init actor network
-                actor_network = ActorNetwork(STATE_DIM, 40, ACTION_DIM)
+                actor_network = ActorNetwork(input_size=STATE_DIM, hidden_size=HIDDEN_SIZE, action_size=ACTION_DIM)
+                actor_network.to(DEVICE)
                 actor_network_optim = torch.optim.Adam(
                     actor_network.parameters(), lr=0.001)
 
@@ -146,24 +159,24 @@ def main():
                 for step in range(STEP):
                     # collect data for n-step rollouts
                     states, actions, rewards, v_final_state, current_state = roll_out(
-                        actor_network, value_network, task, SAMPLE_NUMS, init_state)
+                        actor_network, value_network, env, STEP_ROLLOUT, init_state, ACTION_DIM)
                     init_state = current_state
-                    actions_var = Variable(torch.Tensor(actions).view(-1, ACTION_DIM))
-                    states_var = Variable(torch.Tensor(states).view(-1, STATE_DIM))
+                    actions_var = torch.tensor(actions, dtype=torch.float, device=DEVICE).view(-1, ACTION_DIM)
+                    states_var = torch.tensor(states, dtype=torch.float, device=DEVICE).view(-1, STATE_DIM)
 
                     # train actor network
                     actor_network_optim.zero_grad()
                     log_softmax_actions = actor_network(states_var)
                     vs = value_network(states_var).detach()
-                    
+
                     # calculate qs
-                    qs = Variable(torch.Tensor(
-                        discount_reward(rewards, 0.99, v_final_state)))
+                    qs = torch.tensor(
+                        discount_reward(rewards, 0.99, v_final_state), dtype=torch.float, device=DEVICE)
 
                     advantages = qs - vs
                     actor_network_loss = - \
                         torch.mean(torch.sum(log_softmax_actions *
-                                            actions_var, 1) * advantages)
+                                             actions_var, dim=1) * advantages)
                     actor_network_loss.backward()
                     torch.nn.utils.clip_grad_norm(actor_network.parameters(), 0.5)
                     actor_network_optim.step()
@@ -173,39 +186,40 @@ def main():
                     target_values = qs
                     values = value_network(states_var)
                     criterion = nn.MSELoss()
-                    value_network_loss = criterion(values, target_values)
+                    value_network_loss = criterion(values, target_values.unsqueeze(1))
                     value_network_loss.backward()
                     torch.nn.utils.clip_grad_norm(value_network.parameters(), 0.5)
                     value_network_optim.step()
 
                     # Testing
-                    if (step + 1) % TEST_INTERVAL == 0:
+                    with torch.no_grad():
+                        if (step + 1) % TEST_INTERVAL == 0:
                             test_rewards = []
                             for _ in range(N_TEST_EPI):
-                                
-                                test_state = test_task.reset()
+
+                                test_state = test_env.reset()
                                 test_ep_reward = 0
                                 test_done = False
 
                                 # every test episdoe
                                 while not test_done:
-                                    
+
                                     # get action
                                     test_softmax_action = torch.exp(
-                                        actor_network(Variable(torch.Tensor([test_state]))))
+                                        actor_network(torch.tensor([test_state], dtype=torch.float, device=DEVICE)))
                                     test_action = np.argmax(
-                                        test_softmax_action.data.numpy()[0])
-                                    
+                                        test_softmax_action.cpu().numpy()[0])
+
                                     # take action
-                                    test_next_state, test_reward, test_done, _ = test_task.step(
+                                    test_next_state, test_reward, test_done, _ = test_env.step(
                                         test_action)
-                                    
+
                                     test_ep_reward += test_reward
                                     test_state = test_next_state
-                                    
+
                                     if test_done:
                                         break
-                                
+
                                 # collect rewards for this testing
                                 test_rewards.append(test_ep_reward)
 
@@ -213,8 +227,8 @@ def main():
                             total_test_rewards.append(test_rewards)
 
                             print("Step:", step+1, "Average test rewards:", np.mean(test_rewards))
-                    
-                np.save(result_name, np.array(total_test_rewards))
+
+                np.save(result_file_name, np.array(total_test_rewards))
 
 
 if __name__ == '__main__':
