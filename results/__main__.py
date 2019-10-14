15ead28031c253e52b03b232c859d95e740e52c6
Vincent Wen <x.wen@student.tue.nl>
2019-10-14 13:11:04 +0200

    Update results and visualization.

--- results/__main__.py
+++ results/__main__.py
@@ -37,21 +37,29 @@ def main():
             elif file_name[0] == 'Acrobot-v1':
                 Acrobot[int(file_name[1])][int(file_name[2][:-4])] = np.load(os.path.join('results/', file_))
 
-    # for ENV, STEP_ROLLOUTS in zip(["CartPole-v0", "MountainCar-v0", 'Acrobot-v1'], [[1, 10, 40, 80, 150, 200], [1, 10, 40, 80, 150, 200], [1, 30, 60, 100, 300, 500]]):
-    ENV = 'CartPole-v0'
-    STEP_ROLLOUTS = [1, 10, 40, 80, 150, 200]
-    for SEED in [42, 36, 15]:
-        plt.figure(figsize=(10, 5))
-        for STEP_ROLLOUT in STEP_ROLLOUTS:
-            mean = CartPole[SEED][STEP_ROLLOUT].mean(axis=1)
-            std = CartPole[SEED][STEP_ROLLOUT].std(axis=1)
-            plt.plot(mean)
-            plt.fill_between(np.arange(100), mean - std, mean + std, alpha=0.3, label='n={}'.format(STEP_ROLLOUT))
-        plt.xlabel('Step (x100)')
-        plt.ylabel('Average Return')
-        plt.title('{} Seed={}'.format(ENV, SEED))
-        plt.legend()
-        plt.show()
+    for ENV, STEP_ROLLOUTS in zip(["CartPole-v0", "MountainCar-v0", 'Acrobot-v1'], [[1, 10, 40, 80, 150, 200], [1, 10, 40, 80, 150, 200], [1, 30, 60, 100, 300, 500]]):
+        # ENV = 'CartPole-v0'
+        # STEP_ROLLOUTS = [1, 10, 40, 80, 150, 200]
+        for SEED in [42, 36, 15]:
+            plt.figure(figsize=(10, 5))
+            for STEP_ROLLOUT in STEP_ROLLOUTS:
+                if ENV == 'CartPole-v0':
+                    mean = CartPole[SEED][STEP_ROLLOUT].mean(axis=1)
+                    std = CartPole[SEED][STEP_ROLLOUT].std(axis=1)
+                elif ENV == 'MountainCar-v0':
+                    mean = MountainCar[SEED][STEP_ROLLOUT].mean(axis=1)
+                    std = MountainCar[SEED][STEP_ROLLOUT].std(axis=1)
+                elif ENV == 'Acrobot-v1':
+                    mean = Acrobot[SEED][STEP_ROLLOUT].mean(axis=1)
+                    std = Acrobot[SEED][STEP_ROLLOUT].std(axis=1)
+
+                plt.plot(mean)
+                plt.fill_between(np.arange(100), mean - std, mean + std, alpha=0.3, label='n={}'.format(STEP_ROLLOUT))
+            plt.xlabel('Step (x100)')
+            plt.ylabel('Average Return')
+            plt.title('{} Seed={}'.format(ENV, SEED))
+            plt.legend()
+            plt.show()
 
 
 if __name__ == '__main__':
